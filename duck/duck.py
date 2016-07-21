35672059d64f2a09f8f0452b11f84c58ddcd6f3c
milossimic <milossimicsimo@gmail.com>
2016-07-21 15:54:40 +0200

    input and formating input

--- duck/duck.py
+++ duck/duck.py
@@ -1,1 +1,19 @@
-DB_PEDIA = "http://dbpedia.org/projects/{}.rdf"
+from surf import *
+
+store = Store(  reader='rdflib',
+            writer='rdflib',
+            rdflib_store = 'IOMemory')
+
+#DB PEDIA RDF LINK
+DB_PEDIA = "http://dbpedia.org/projects/{}"
+RDF=".rdf"
+session = Session(store)
+
+#ENTER NAME AND FORM A PATH
+testVar = raw_input("Hurricane name:")
+hurricane_name = DB_PEDIA.format(testVar+RDF)
+
+#PRINT :)
+print hurricane_name
+
+#store.load_triples(source=hurricane_name)
