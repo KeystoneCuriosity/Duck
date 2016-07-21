6356b41a8ca5dcecc74807b8ab067a8f5e9e72b0
milossimic <milossimicsimo@gmail.com>
2016-07-21 18:25:31 +0200

    djubre radi

--- duck/duck.py
+++ duck/duck.py
@@ -1,19 +1,22 @@
-from surf import *
+from SPARQLWrapper import SPARQLWrapper, JSON
 
-store = Store(  reader='rdflib',
-            writer='rdflib',
-            rdflib_store = 'IOMemory')
+def get_country_description():
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setReturnFormat(JSON)
 
-#DB PEDIA RDF LINK
-DB_PEDIA = "http://dbpedia.org/projects/{}"
-RDF=".rdf"
-session = Session(store)
+    sparql.setQuery(query)  # the previous query as a literal string
 
-#ENTER NAME AND FORM A PATH
-testVar = raw_input("Hurricane name:")
-hurricane_name = DB_PEDIA.format(testVar+RDF)
+    return sparql.query().convert()
 
-#PRINT :)
-print hurricane_name
+    from SPARQLWrapper import SPARQLWrapper, JSON
 
-#store.load_triples(source=hurricane_name)
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql.setQuery("""
+    PREFIX dbp: <http://dbpedia.org/property/>
+    SELECT ?areas
+    WHERE { <http://dbpedia.org/resource/Hurricane_Katrina> dbp:areas ?areas }
+""")
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+print results
