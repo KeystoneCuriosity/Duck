92a3b5dee03a33d3e0d400e677dd2b955ca8a300
milossimic <milossimicsimo@gmail.com>
2016-07-22 03:44:37 +0200

    abstract, state and city administration level 1

--- duck/duck.py
+++ duck/duck.py
@@ -1,7 +1,12 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 from geopy.geocoders import Nominatim
+import geonames.adapters.search
+import json, urllib
+import rdflib
 
 geolocator = Nominatim()
+_USERNAME = 'milossimic'
+sa = geonames.adapters.search.Search(_USERNAME)
 
 def get_country_description():
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
@@ -13,7 +18,24 @@ def get_country_description():
 
     from SPARQLWrapper import SPARQLWrapper, JSON
 
+#ABSTRACT
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql.setQuery("""
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    SELECT ?abstract
+    WHERE { <http://dbpedia.org/resource/Hurricane_Katrina> dbo:abstract ?abstract }
+""")
+sparql.setReturnFormat(JSON)
+abstract = sparql.query().convert()
+
+for a in abstract['results']['bindings']:
+	if a['abstract']['xml:lang'] == 'en':
+		print a['abstract']['value']
+
+print
+print
+
+#COUNTRY AND PROVINCE LEVEL 1
 sparql.setQuery("""
     PREFIX dbp: <http://dbpedia.org/property/>
     SELECT ?areas
@@ -27,5 +49,48 @@ for result in results["results"]["bindings"]:
 		try:
 			location = geolocator.geocode(line.strip())
 			print(location.latitude, location.longitude), line.strip()
+
+			url = 'http://api.geonames.org/search?q={}&type=json&username=milossimic'.format(line.strip())
+
+			response = urllib.urlopen(url)
+			data = json.loads(response.read())
+			countryId = data['geonames'][0]['countryId']
+
+			provinces = 'http://sws.geonames.org/{}/contains.rdf'.format(countryId)
+
+			g = rdflib.Graph()
+			g.parse(provinces)
+
+			qres = g.query(
+				"""
+				PREFIX gn:<http://www.geonames.org/ontology#>
+				SELECT ?name
+				WHERE {?x gn:name ?name}""")
+
+			for q in qres:
+				print "\t",q[0]
+
+			print
+
 		except AttributeError:
 			print line.strip()
+
+# url = 'http://api.geonames.org/search?q=Cuba&type=json&username=milossimic'
+
+# response = urllib.urlopen(url)
+# data = json.loads(response.read())
+# print data['geonames'][0]['countryId']
+
+# provinces = 'http://sws.geonames.org/3562981/contains.rdf'
+
+# g = rdflib.Graph()
+# g.parse(provinces)
+
+# qres = g.query(
+# 	"""
+# 	PREFIX gn:<http://www.geonames.org/ontology#>
+# 	SELECT ?name
+# 	WHERE {?x gn:name ?name}""")
+
+# for q in qres:
+# 	print q[0]
